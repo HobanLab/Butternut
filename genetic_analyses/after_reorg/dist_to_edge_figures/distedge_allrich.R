666a7f3714d2560ae82c66b3fb8c5fb3d6fb3d7d
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-04-05 17:33:15 -0500

    Update distedge_allrich.R
    updating allelic richness with pathways

--- genetic_analyses/after_reorg/dist_to_edge_figures/distedge_allrich.R
+++ genetic_analyses/after_reorg/dist_to_edge_figures/distedge_allrich.R
@@ -4,20 +4,10 @@
 
 library(diveRsity)
 library(adegenet)
-library(stringr)
 library(tidyr)
 library(hierfstat)
 library(poppr)
 library(Demerelate)
-library(rworldmap)
-library(data.table)
-library(ggplot2)
-library(ggrepel)
-library(geosphere)
-library(plotrix)
-library(ggpmisc)
-library(factoextra)
-library(GISTools)
 library(raster)
 library(rgdal)
 library(sp)
@@ -25,18 +15,15 @@ library(sp)
 #####################################
 ############ Directories ############
 #####################################
-dist_edge_path <- "G:\\Shared drives\\Emily_Schumacher\\Butternut\\butternut_publication_figures\\dist_edge\\"
 butternut_drive <- "G:\\My Drive\\Hoban_Lab_Docs\\Projects\\Butternut_JUCI\\"
 
 ##load butternut
-setwd(dist_edge_path)
-butternut_buffer <- shapefile("butternut_buffer")
+setwd(butternut_drive)
+butternut_buffer <- shapefile("Graphical_Stat_Results\\PostIndRemoval\\GeographicImages\\butternut_buffer")
 
 ##############################################################
 #################### Load Genetic Files  #####################
 ##############################################################
-setwd(butternut_drive)
-
 ##load current working reorganized genepop file 
 butternutgen_reorg <- read.genepop("DataFiles\\24Populations\\reorg\\reorg_gen_24pop.gen", ncode = 3)
 
@@ -58,8 +45,8 @@ levels(butternutgen_reorg@pop) <- butternut_24pop_names
 ##calculate poppr
 butternut_24pop_poppr <- poppr(butternutgen_reorg)
 
-##get distance to range edge for each pop
-dist_edge_df <- read.csv(paste0(dist_edge_path, "dist_edge_df.csv"))
+##dist edge df 
+dist_edge_df <- read.csv("Graphical_Stat_Results\\PostIndRemoval\\GeographicImages\\dist_edge_df.csv")
 
 ######################################################################
 #################### Run Analyses and Create DF  #####################
@@ -77,7 +64,6 @@ allrich_red <- all_rich_dist_df[-c(16,22,23),]
 ######################################################################
 ########################## Linear Models  ############################
 ######################################################################
-
 ###now calculate regressions - linear
 allrich_lm <- lm(all_rich_dist_df[,2]~all_rich_dist_df[,1])
 allrich_lm_sum <- summary(allrich_lm)
@@ -111,7 +97,7 @@ linear_allrich_dist_red_rp[2] <- substitute(expression(italic(p) == MYOTHERVALUE
 ##################################################################
 
 ##Allelic Richness Distance
-pdf(paste0(dist_edge_path, "linear_allrich_dist.pdf"), width = 8, height = 6)
+pdf("Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\linear_allrich_dist.pdf", width = 8, height = 6)
 
 plot(all_rich_dist_df[,2]~all_rich_dist_df[,1], 
      col = as.character(all_rich_dist_df[,3]), pch = 17,
@@ -120,37 +106,34 @@ plot(all_rich_dist_df[,2]~all_rich_dist_df[,1],
      main = "Number of Alleles Compared with Distance to Range Edge (km)")
 
 text(all_rich_dist_df[,2]~all_rich_dist_df[,1], 
-     labels = c(1:24), cex = 0.8, pos = 1)
+     labels = rownames(all_rich_dist_df), cex = 0.8, pos = 1)
 
 abline(allrich_lm, col = "dodgerblue4", lwd = 3)
 abline(allrich_red_lm, col = "darkorchid", lwd = 3)
 
 ##legends
-legend('topleft', legend = linear_allrich_dist_rp, pch = 4, col = "dodgerblue4",
+legend('topleft', legend = linear_allrich_dist_rp, pch = 17, col = "dodgerblue4",
        title = "Regression with WI",
        bty = 'n', border = "black", pt.cex = 1, cex = 0.8)
 
-legend('bottomleft', legend = linear_allrich_dist_red_rp, pch = 4, 
+legend('bottomleft', legend = linear_allrich_dist_red_rp, pch = 17, 
        col = "darkorchid", title = "Regression without WI",
        bty = 'n', border = "black", pt.cex = 1, cex = 0.8)
 
-legend('bottom', legend = c("New Brunswick", "Ontario", "United States"), pch = 17, col = c("firebrick1", "firebrick4","dodgerblue"))
+legend('bottom', legend = c("New Brunswick", "Ontario", "United States"), pch = 17, 
+       col = c("firebrick1", "firebrick4","dodgerblue"))
 
 dev.off()
 
 ##create data frame of pvalues r2 for linear and quad models
-dist_edge_allrich_df <- matrix(nrow = 4, ncol = 2)
+dist_edge_allrich_df <- matrix(nrow = 2, ncol = 2)
 rownames(dist_edge_allrich_df) <- c("linear_allrich_dist_edge",
-                                    "linear_allrich_dist_edge_woWI",
-                                    "quadratic_allrich_dist_edge",
-                                    "quadratic_allrich_dist_edge_woWI")
+                                    "linear_allrich_dist_edge_woWI")
 colnames(dist_edge_allrich_df) <- c("Pvalue","R2")
 ##write values
 dist_edge_allrich_df[1,] <- c(linear_allrich_dist_pvalue, linear_allrich_dist_r2)
 dist_edge_allrich_df[2,] <- c(linear_allrich_dist_red_pvalue, linear_allrich_dist_red_r2)
-dist_edge_allrich_df[3,] <- c(quad_allrich_dist_pvalue, quad_allrich_dist_r2)
-dist_edge_allrich_df[4,] <- c(quad_allrich_dist_red_pvalue, quad_allrich_dist_red_r2)
 
 ##write out
-write.csv(dist_edge_allrich_df, paste0("G:\\Shared drives\\Emily_Schumacher\\butternut_publication_figures\\pvalue_r2_dist_edge_allrich.csv"))
+write.csv(dist_edge_allrich_df, paste0("Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\pvalue_r2_dist_edge_allrich.csv"))
 
