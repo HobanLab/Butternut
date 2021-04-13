a3a42ffb05d50abe9d2682d29aeee6706f5ee718
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-04-13 12:57:40 -0500

    Update sum_stats_nall_div.R
    Create code for putting on allelic statistics together in one table

--- genetic_analyses/after_reorg/sum_stats_nall_div.R
+++ genetic_analyses/after_reorg/sum_stats_nall_div.R
@@ -6,6 +6,7 @@ library(adegenet)
 library(poppr)
 library(pegas)
 library(PopGenReport)
+library(hierfstat)
 
 #####################################
 ############# Load Files ############
@@ -35,9 +36,58 @@ bn_sumstats <- summary(butternutgen_reorg)
 ##create poppr file 
 BN_poppr <- poppr(butternutgen_reorg)
 
-##calculate heterozygosity
+##expected heterozygosity 
 BN_hexp <- BN_poppr[1:24, 10]
-bn_hexp_df <- data.frame(butternut_24pop_names,BN_hexp)
+##allele numbers by pop 
+BN_nall <- bn_sumstats$pop.n.all
+##individual numbers
+BN_ind <- BN_poppr[1:24, 2:3]
+##allelic richness code 
+BN_alleles<-bn_sumstats$pop.n.all/length(butternutgen_reorg@loc.n.all)
+BN_all_rich<-colSums(allelic.richness(butternutgen_reorg)$Ar)/length(butternutgen_reorg@loc.n.all)	#remember to divide by number of loci!!
+
+##Calculate mean latitude and longitude for every population
+##now do lon lat calcs
+butternut_mean_lon <- matrix()
+butternut_mean_lat <- matrix()
+
+##loops for mean lat/lon
+for(pop in butternut_24pop_names){
+  
+  butternut_mean_lon[pop] <- mean(butternut_reorg_lonlat[butternut_reorg_lonlat$Pop == pop,][,3])  
+  
+}
+
+for(pop in butternut_24pop_names){
+  
+  butternut_mean_lat[pop] <- mean(butternut_reorg_lonlat[butternut_reorg_lonlat$Pop == pop,][,4])  
+  
+}
+
+##convert to matrix
+butternut_mean_lon <- matrix(butternut_mean_lon)
+butternut_mean_lat <- matrix(butternut_mean_lat)
+
+##document cleanup
+butternut_mean_lon <- butternut_mean_lon[-1]
+butternut_mean_lat <- butternut_mean_lat[-1] 
+
+##create pop names 
+butternut_24pop_names <- unique(butternut_reorg_lonlat$Pop)
+
+##create data frame 
+butternut_stat_df <- signif(cbind(butternut_mean_lon, butternut_mean_lat, BN_ind, BN_nall, BN_all_rich, BN_hexp),3)
+
+##name columns and rows 
+rownames(butternut_stat_df) <- butternut_24pop_names
+colnames(butternut_stat_df) <- c("Mean Longitude", "Mean Latitude", "Number of Individuals", "MLG","Number of Alleles", "Allelic Richness", "Expected Heterozygosity")
+
+##write out csv 
+write.csv(butternut_stat_df, "Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\butternut_stat_df.csv")
+
+##############################
+####### Run Checks ###########
+##############################
 
 ##calculate null alleles 
 bn_null_all <- null.all(butternutgen_reorg)
