df5666b67819fc2b1911c217a9760127a29a3425
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-04-20 15:13:34 -0500

    Update allrich_lat_woNB_wNB.R
    without NB quadratic equation

--- genetic_analyses/latitude_figures/allrich_lat_woNB_wNB.R
+++ genetic_analyses/latitude_figures/allrich_lat_woNB_wNB.R
@@ -2,92 +2,41 @@
 ######## Libraries #######
 ##########################
 
-library(diveRsity)
 library(adegenet)
-library(tidyr)
 library(hierfstat)
 library(poppr)
-library(PopGenReport)
 
 #####################################
 ############ Directories ############
 #####################################
-butternut_drive <- "G:\\My Drive\\Hoban_Lab_Docs\\Projects\\Butternut_JUCI"
+butternut_drive <- "C:\\Users\\eschumacher\\Documents\\GitHub\\butternut"
 
 #####################################
 ############# Load Files ############
 #####################################
+##set working directory for files 
 setwd(butternut_drive)
 
 ##load current working reorganized genepop file 
-butternutgen_reorg <- read.genepop("DataFiles\\24Populations\\reorg\\reorg_gen_24pop.gen", ncode = 3)
+butternutgen_reorg <- read.genepop("data_files\\after_reorg\\reorg_gen_24pop.gen", ncode = 3)
 
 ##load relatedness
-reorg_relatedness <- read.csv("DataFiles\\24Populations\\reorg\\reorg_relatedness.csv")
+reorg_relatedness <- read.csv("data_files\\after_reorg\\reorg_relatedness.csv")
 
 ##rename genind 
 rownames(butternutgen_reorg@tab) <- reorg_relatedness$Ind
 
-##Lon lat files 
-butternut_reorg_lonlat <- read.csv("DataFiles\\24Populations\\reorg\\reorg_lon_lat.csv")
+##load in mean longitude and latitude by population 
+all_rich_lat_df <- read.csv("genetic_analyses_results\\all_rich_lat_df.csv")
 
-##pop name
-butternut_24pop_names <- unique(butternut_reorg_lonlat$Pop)
+##get pop names 
+butternut_24pop_names <- all_rich_lat_df[,1]
+
+##name populations in genind file 
+levels(butternutgen_reorg@pop) <- butternut_24pop_names
 
 ##create butternut poppr
 butternut_poppr <- poppr(butternutgen_reorg)
-
-#############################################
-############# Mean Lat and Long #############
-#############################################
-
-##Calculate mean latitude and longitude for every population
-##now do lon lat calcs
-butternut_mean_lon <- matrix()
-butternut_mean_lat <- matrix()
-
-##loops for mean lat/lon
-for(pop in butternut_24pop_names){
-  
-  butternut_mean_lon[pop] <- mean(butternut_reorg_lonlat[butternut_reorg_lonlat$Pop == pop,][,3])  
-  
-}
-
-for(pop in butternut_24pop_names){
-  
-  butternut_mean_lat[pop] <- mean(butternut_reorg_lonlat[butternut_reorg_lonlat$Pop == pop,][,4])  
-  
-}
-
-##convert to matrix
-butternut_mean_lon <- matrix(butternut_mean_lon)
-butternut_mean_lat <- matrix(butternut_mean_lat)
-
-##document cleanup
-butternut_mean_lon <- butternut_mean_lon[-1]
-butternut_mean_lat <- butternut_mean_lat[-1]
-
-##create min and max documents
-min_lon <- min(butternut_mean_lon)
-max_lon <- max(butternut_mean_lon)
-min_lat <- min(butternut_mean_lat)
-max_lat <- max(butternut_mean_lat)
-
-##########################################################
-################ Genetic Diversity Plotting ##############
-##########################################################
-################Allelic Richness
-##Calculate Allelic Richness
-reorg_allrich <- colSums(allelic.richness(butternutgen_reorg)$Ar)/length(butternutgen_reorg@loc.n.all)
-
-##create df with allelic richness and latitude
-all_rich_lat_df <- data.frame(butternut_mean_lat, reorg_allrich)
-colnames(all_rich_lat_df) <- c("Mean_Lat", "Allelic_Richness")
-rownames(all_rich_lat_df) <- butternut_24pop_names
-all_rich_lat_df$Color <- NA
-all_rich_lat_df[1:6,3] <- "firebrick1"
-all_rich_lat_df[7:11,3] <- "firebrick4"
-all_rich_lat_df[12:24,3] <- "dodgerblue"
 
 ##########################################################
 #################### Separate NB  ########################
@@ -100,14 +49,14 @@ all_rich_lat_df_NB <- all_rich_lat_df[c(1:6),]
 ########## Linear Modeling without NB  ###################
 ##########################################################
 ##Create linear relationship with lat and number of alleles 
-allrich_lm <- lm(all_rich_lat_df_wo_NB[,2]~all_rich_lat_df_wo_NB[,1])
+allrich_lm <- lm(all_rich_lat_df_wo_NB[,3]~all_rich_lat_df_wo_NB[,2])
 allrich_lm_sum <- summary(allrich_lm)
 
 ###df reduced without wisconsin
 all_rich_lat_df_wo_NB_red <- all_rich_lat_df_wo_NB[-c(10,16:17),]
 
 ##Now create linear model 
-allrich_red_lm <- lm(all_rich_lat_df_wo_NB_red[,2]~all_rich_lat_df_wo_NB_red[,1])
+allrich_red_lm <- lm(all_rich_lat_df_wo_NB_red[,3]~all_rich_lat_df_wo_NB_red[,2])
 allrich_red_lm_sum <- summary(allrich_red_lm)
 
 ##create r2 and p values
@@ -131,15 +80,15 @@ allrich_red_rp[2] = substitute(expression(italic(p) == MYOTHERVALUE),
                                list(MYOTHERVALUE = format(allrich_red_pvalue, digits = 2)))[2]
 
 ##Allelic Richness Linear
-pdf("Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\linear_woNB_all_rich_Lat.pdf", width = 8, height = 6)
+pdf("genetic_analyses_results\\linear_woNB_all_rich_Lat.pdf", width = 8, height = 6)
 
 ##first plot
-plot(all_rich_lat_df_wo_NB[,2]~all_rich_lat_df_wo_NB[,1], col = all_rich_lat_df_wo_NB[,3], pch = 17, 
-     main = "Number of Alleles Compared to Mean Latitude", ylab = "Number of Alleles", 
-     xlab = "Mean Latitude", cex = (butternut_poppr[7:24,2]/80), ylim = c(5,10))
+plot(all_rich_lat_df_wo_NB[,3]~all_rich_lat_df_wo_NB[,2], col = all_rich_lat_df_wo_NB[,4], pch = 17, 
+     main = "Allelic Richness Compared to Mean Latitude", ylab = "Allelic Richness", 
+     xlab = "Mean Latitude", cex = (butternut_poppr[7:24,2]/50), ylim = c(5,10))
 
 ##name Ontario and US
-text(all_rich_lat_df_wo_NB[,2]~all_rich_lat_df_wo_NB[,1], labels = rownames(all_rich_lat_df_wo_NB), 
+text(all_rich_lat_df_wo_NB[,3]~all_rich_lat_df_wo_NB[,2], labels = all_rich_lat_df_wo_NB[,1], 
      cex = 0.8, pos = 1)
 
 ##plot linear models
@@ -151,14 +100,15 @@ legend('topleft', legend = allrich_rp, bty = 'n', border = "black", pt.cex = 1, 
        col = "dodgerblue4", title = "With WI")
 legend('bottomleft', legend = allrich_red_rp, bty = 'n', border = "black", pt.cex = 1, cex = 0.8, pch = 17, 
        col = "darkorchid4", title = "Without WI")
-legend('bottom', legend = c("Ontario", "United States"), pch = 17, col = c("firebrick4","dodgerblue"))
+legend('bottom', legend = c("Ontario", "Quebec", "United States"), pch = 17, col = c("firebrick4", "lightsalmon",
+                                                                                     "dodgerblue"))
 dev.off()
 
 ##########################################################
 ################# Linear Modeling NB  ####################
 ##########################################################
 ##Create linear relationship with lat and number of alleles 
-allrich_NB_lm <- lm(all_rich_lat_df_NB[,2]~all_rich_lat_df_NB[,1])
+allrich_NB_lm <- lm(all_rich_lat_df_NB[,3]~all_rich_lat_df_NB[,2])
 allrich_NB_lm_sum <- summary(allrich_NB_lm)
 
 ##create r2 and p values
@@ -172,15 +122,15 @@ allrich_NB_rp[2] = substitute(expression(italic(p) == MYOTHERVALUE),
                            list(MYOTHERVALUE = format(allrich_NB_pvalue, digits = 2)))[2]
 
 ##Allelic Richness Linear
-pdf("Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\linear_NB_all_rich_Lat.pdf", width = 8, height = 6)
+pdf("genetic_analyses_results\\linear_NB_all_rich_Lat.pdf", width = 8, height = 6)
 
 ##first plot
-plot(all_rich_lat_df_NB[,2]~all_rich_lat_df_NB[,1], col = all_rich_lat_df_NB[,3], pch = 17, 
+plot(all_rich_lat_df_NB[,3]~all_rich_lat_df_NB[,2], col = all_rich_lat_df_NB[,4], pch = 17, 
      main = "Number of Alleles Compared to Mean Latitude", ylab = "Number of Alleles", 
      xlab = "Mean Latitude", cex = (butternut_poppr[1:6,2]/80), ylim = c(5,10))
 
 ##name Ontario and US
-text(all_rich_lat_df_NB[,2]~all_rich_lat_df_NB[,1], labels = rownames(all_rich_lat_df_NB), 
+text(all_rich_lat_df_NB[,3]~all_rich_lat_df_NB[,2], labels = all_rich_lat_df_NB[,1], 
      cex = 0.8, pos = 1)
 
 ##plot linear models
@@ -196,13 +146,12 @@ dev.off()
 ##########################################################
 ################### Quadratic wo NB ######################
 ##########################################################
-##Do quadratic
 #calculate data points 
-wo_NB_points <- all_rich_lat_df_wo_NB[,1]
+wo_NB_points <- all_rich_lat_df_wo_NB[,2]
 wo_NB_points2 <- wo_NB_points^2
 
 ###Quadratic linear model
-wo_NB_quadratic_model_lm <-lm(all_rich_lat_df_wo_NB[,2] ~ 
+wo_NB_quadratic_model_lm <-lm(all_rich_lat_df_wo_NB[,3] ~ 
                                 wo_NB_points + wo_NB_points2)
 ##Calculate summary documents 
 wo_NB_quadratic_model_lm_sum <- summary(wo_NB_quadratic_model_lm)
@@ -215,17 +164,17 @@ wo_NB_points_counts <- predict(wo_NB_quadratic_model_lm,
                                     wo_NB_points2 = wo_NB_points_values^2))
 
 ###########without Wisconsin populations
-wo_NB_points_red <- all_rich_lat_df_wo_NB_red[,1]
-wo_NB_points2_red <- all_rich_lat_df_wo_NB_red[,1]^2
+wo_NB_points_red <- all_rich_lat_df_wo_NB_red[,2]
+wo_NB_points2_red <- wo_NB_points_red^2
 
 ##create quadratic regression documents 
-quadratic_wo_NB_red_lm <-lm(all_rich_lat_df_wo_NB_red[,2] ~ 
+quadratic_wo_NB_red_lm <-lm(all_rich_lat_df_wo_NB_red[,3] ~ 
                               wo_NB_points_red + wo_NB_points2_red)
 quadratic_wo_NB_red_lm_sum <- summary(quadratic_wo_NB_red_lm)
 
 ##values and pointsance
-wo_NB_points_values_red <- seq(min(all_rich_lat_df_wo_NB_red[,1]), 
-                               max(all_rich_lat_df_wo_NB_red[,1]), 0.5)
+wo_NB_points_values_red <- seq(min(all_rich_lat_df_wo_NB_red[,2]), 
+                               max(all_rich_lat_df_wo_NB_red[,2]), 0.5)
 wo_NB_points_counts_red <- predict(quadratic_wo_NB_red_lm,list(wo_NB_points_red = wo_NB_points_values_red, 
                                                                wo_NB_points2_red = wo_NB_points_values_red^2))
 
@@ -250,26 +199,24 @@ wo_NB_allrich_quad_red_rp[2] = substitute(expression(italic(p) == MYOTHERVALUE),
                                     list(MYOTHERVALUE = format(wo_NB_allrich_quad_red_pvalue, digits = 2)))[2]
 
 ########Now Plot quadatric
-pdf("Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\wo_NB_allrich_quad.pdf", width = 8, height = 6)
+pdf("genetic_analyses_results\\wo_NB_allrich_quad.pdf", width = 8, height = 6)
 
 ###Plot same thing 
-plot(all_rich_lat_df_wo_NB[,2]~all_rich_lat_df_wo_NB[,1], col = all_rich_lat_df_wo_NB[,3], 
-     pch = 17, main = "Number of Alleles Compared to Mean Latitude", ylab = "Number of Alleles", 
+plot(all_rich_lat_df_wo_NB[,3]~all_rich_lat_df_wo_NB[,2], col = all_rich_lat_df_wo_NB[,4], 
+     pch = 17, main = "Allelic Richness Compared to Mean Latitude", ylab = "Allelic Richness", 
      xlab = "Mean Latitude", xlim = c(35, 47),
-     cex = (butternut_poppr[7:24,2]/100), ylim = c(5,10))
-points(all_rich_lat_df_NB[,2]~all_rich_lat_df_NB[,1], col = all_rich_lat_df_NB[,3],
-       pch = 17)
+     cex = (butternut_poppr[7:24,2]/50), ylim = c(5,10))
 
 ##Add text
-text(all_rich_lat_df[,2]~all_rich_lat_df[,1], labels = butternut_24pop_names, cex = 0.8, pos = 1)
+text(all_rich_lat_df_wo_NB[,3]~all_rich_lat_df_wo_NB[,2], labels = all_rich_lat_df_wo_NB[,1], cex = 0.8, pos = 1)
 
 ##Now plot lines 
 lines(wo_NB_points_values, wo_NB_points_counts, col = "darkslategray3", lwd = 3)
 lines(wo_NB_points_values_red, wo_NB_points_counts_red, col = "darkseagreen4", lwd = 3)
 
 ##Create legends 
-legend('bottom', legend = c("New Brunswick", "Ontario", "United States"), pch = 17, 
-       col = c("firebrick1", "firebrick4","dodgerblue"))
+legend('bottom', legend = c("Ontario", "Quebec", "United States"), pch = 17, 
+       col = c("firebrick4", "lightsalmon","dodgerblue"))
 legend('topleft', legend = wo_NB_allrich_quad_rp, bty = 'n', border = "black", pt.cex = 1, cex = 0.8, pch = 17, 
        col = "darkslategray3", title = "Quad Regression wo NB")
 legend('bottomleft', legend = wo_NB_allrich_quad_red_rp, bty = 'n', border = "black", pt.cex = 1, cex = 0.8, 
@@ -292,7 +239,7 @@ allrich_r2_pvalue_quad_linear_wo_NB_df[4,] <- c(wo_NB_allrich_quad_red_pvalue, w
 allrich_r2_pvalue_quad_linear_wo_NB_df[5,] <- c(allrich_NB_pvalue,allrich_NB_r2)
 
 ##write out table
-write.csv(allrich_r2_pvalue_quad_linear_wo_NB_df,"Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\allrich_r2_pvalue_quad_linear_wo_NB_df.csv")
+write.csv(allrich_r2_pvalue_quad_linear_wo_NB_df,"genetic_analyses_results\\allrich_r2_pvalue_quad_linear_wo_NB_df.csv")
 
 
 
