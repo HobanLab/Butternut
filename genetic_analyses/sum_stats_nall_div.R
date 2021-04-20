ac8573bf8551f084c18dd1fd2d42b2536fc83e41
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-04-20 13:00:46 -0500

    Update sum_stats_nall_div.R
    updating summary statistics for genetic diversity code

--- genetic_analyses/sum_stats_nall_div.R
+++ genetic_analyses/sum_stats_nall_div.R
@@ -4,28 +4,35 @@
 
 library(adegenet)
 library(poppr)
-library(pegas)
-library(PopGenReport)
 library(hierfstat)
 
-#####################################
-############# Load Files ############
-#####################################
-##set working directory
-butternut_dir <- "G:/My Drive/Hoban_Lab_Docs/Projects/Butternut_JUCI"
-setwd(butternut_dir)
+##########################################
+############# Set directories ############
+##########################################
+##set directory to all butternut files 
+butternut_drive <- "C:\\Users\\eschumacher\\Documents\\GitHub\\butternut"
+
+##########################################
+############ Load in files ###############
+##########################################
+setwd(butternut_drive)
 
 ##load reorg genind
-butternutgen_reorg <- read.genepop("DataFiles/24Populations/reorg/reorg_gen_24pop.gen", ncode = 3)
+butternutgen_reorg <- read.genepop("data_files\\after_reorg\\reorg_gen_24pop.gen", ncode = 3)
 
-##load lat lon doc 
-butternut_reorg_lonlat <- read.csv("DataFiles/24Populations/reorg/reorg_lon_lat.csv")
+##load lat lon doc for individual names and population names 
+butternut_reorg_lonlat <- read.csv("data_files\\after_reorg\\reorg_lon_lat.csv")
 
-##create name doc
+##load in mean lon and lat document 
+butternut_mean_lon_lat <- read.csv("data_files\\geographic_files\\butternut_coord_df.csv")
+
+##create population name doc
 butternut_24pop_names <- unique(butternut_reorg_lonlat$Pop)
 
-##name genind doc 
+##name individuals in genind doc 
 rownames(butternutgen_reorg@tab) <- butternut_reorg_lonlat$Ind
+##name populations in genind doc 
+levels(butternutgen_reorg@pop) <- butternut_24pop_names
 
 ######################################
 ############# basic stats ############
@@ -43,51 +50,22 @@ BN_nall <- bn_sumstats$pop.n.all
 ##individual numbers
 BN_ind <- BN_poppr[1:24, 2:3]
 ##allelic richness code 
-BN_alleles<-bn_sumstats$pop.n.all/length(butternutgen_reorg@loc.n.all)
-BN_all_rich<-colSums(allelic.richness(butternutgen_reorg)$Ar)/length(butternutgen_reorg@loc.n.all)	#remember to divide by number of loci!!
-
-##Calculate mean latitude and longitude for every population
-##now do lon lat calcs
-butternut_mean_lon <- matrix()
-butternut_mean_lat <- matrix()
-
-##loops for mean lat/lon
-for(pop in butternut_24pop_names){
-  
-  butternut_mean_lon[pop] <- mean(butternut_reorg_lonlat[butternut_reorg_lonlat$Pop == pop,][,3])  
-  
-}
-
-for(pop in butternut_24pop_names){
-  
-  butternut_mean_lat[pop] <- mean(butternut_reorg_lonlat[butternut_reorg_lonlat$Pop == pop,][,4])  
-  
-}
-
-##convert to matrix
-butternut_mean_lon <- matrix(butternut_mean_lon)
-butternut_mean_lat <- matrix(butternut_mean_lat)
-
-##document cleanup
-butternut_mean_lon <- butternut_mean_lon[-1]
-butternut_mean_lat <- butternut_mean_lat[-1] 
-
-##create pop names 
-butternut_24pop_names <- unique(butternut_reorg_lonlat$Pop)
+BN_alleles <-bn_sumstats$pop.n.all/length(butternutgen_reorg@loc.n.all)
+BN_all_rich <- colSums(allelic.richness(butternutgen_reorg)$Ar)/length(butternutgen_reorg@loc.n.all)	
 
 ##create data frame 
-butternut_stat_df <- signif(cbind(butternut_mean_lon, butternut_mean_lat, BN_ind, BN_nall, BN_all_rich, BN_hexp),3)
+butternut_stat_df <- signif(cbind(butternut_mean_lon_lat[,2:3], BN_ind, BN_nall, BN_all_rich, BN_hexp),3)
 
 ##name columns and rows 
 rownames(butternut_stat_df) <- butternut_24pop_names
 colnames(butternut_stat_df) <- c("Mean Longitude", "Mean Latitude", "Number of Individuals", "MLG","Number of Alleles", "Allelic Richness", "Expected Heterozygosity")
 
 ##write out csv 
-write.csv(butternut_stat_df, "Graphical_Stat_Results\\PostIndRemoval\\24pop\\Reorg_Results\\GeneticDiversity\\butternut_stat_df.csv")
+write.csv(butternut_stat_df, "genetic_analyses_results\\butternut_stat_df.csv")
 
-##############################
-####### Run Checks ###########
-##############################
+############################################################################
+####### Run Genetic Diversity Checks like LD, HWE, Null Alleles  ###########
+############################################################################
 
 ##calculate null alleles 
 bn_null_all <- null.all(butternutgen_reorg)
