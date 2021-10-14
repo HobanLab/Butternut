8452db8ee5b4193e6a6b1a8c5ad5779100b124f1
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2021-10-14 10:02:13 -0500

    added PCoA code to the QU supplement script

--- genetic_analyses/5_Supplemental_QU_figures.R
+++ genetic_analyses/5_Supplemental_QU_figures.R
@@ -5,6 +5,8 @@
 library(adegenet)
 library(poppr)
 library(dplyr)
+library(geosphere)
+library(hierfstat)
 
 ##########################################
 ############# Set directories ############
@@ -174,35 +176,58 @@ linear_lat_hexp_red_lm <- lm(butternut_26pop_gendiv_geo_red$HExp~butternut_26pop
 linear_lat_hexp_red_lm_sum <- summary(linear_lat_hexp_red_lm)
 
 ##run quadratic regression on mean lat and hexp 
-
-
-
-
-##without WI 
 #calculate data points 
-lat_points_red <- butternut_26pop_gendiv_geo_red$Mean_Lat
-lat_points2_red <- lat_points_red^2
+quad_lat_hexp_points <- butternut_26pop_gendiv_geo$Mean_Lat
+quad_lat_hexp_points2 <- quad_lat_hexp_points^2
 
 ###Quadratic linear model
-genstat_quad_model_red_lm <-lm(butternut_26pop_gendiv_geo_red$All_Rich ~ 
-                             lat_points_red + lat_points2_red)
+quad_lat_hexp_model_lm <-lm(butternut_26pop_gendiv_geo$HExp ~ 
+                              quad_lat_hexp_points + quad_lat_hexp_points2)
 
 ##Calculate summary documents 
-genstat_quad_model_red_lm_sum <- summary(genstat_quad_model_red_lm)
+quad_lat_hexp_lm_sum <- summary(quad_lat_hexp_model_lm)
 
-##calculate y-axis values 
-quad_values_red <- seq(min(butternut_26pop_gendiv_geo_red[,2]), max(butternut_26pop_gendiv_geo_red[,2]), 0.5)
-quad_counts_red <- predict(genstat_quad_model_red_lm, list(lat_points_red=quad_values_red, lat_points2_red=quad_values_red^2))
+####calculate reduced data frame with the quadratic model 
+quad_lat_hexp_red_points <- butternut_26pop_gendiv_geo_red$Mean_Lat
+quad_lat_hexp_red_points2 <- quad_lat_hexp_red_points^2
 
-##create r2 and p values
-genstat_red_r2 <- genstat_quad_model_red_lm_sum$adj.r.squared
-genstat_red_pvalue <- genstat_quad_model_red_lm_sum$coefficients[2,4]
-genstat_red_rp <- vector('expression',2)
-genstat_red_rp[1] <- substitute(expression(italic(R)^2 == MYVALUE), 
-                            list(MYVALUE = format(genstat_red_r2,dig=3)))[2]
-genstat_red_rp[2] = substitute(expression(italic(p) == MYOTHERVALUE), 
-                           list(MYOTHERVALUE = format(genstat_red_pvalue, digits = 2)))[2]
+##calculate reduced quadratic model 
+quad_lat_hexp_model_red_lm <- lm(butternut_26pop_gendiv_geo_red$HExp ~
+                                   quad_lat_hexp_red_points + quad_lat_hexp_red_points2)
+##made a summary of the regression 
+quad_lat_hexp_model_red_lm_sum <- summary(quad_lat_hexp_model_red_lm)
 
+###########dist to range edge and hexp 
+linear_dist_hexp_lm <- lm(butternut_26pop_gendiv_geo$HExp~butternut_26pop_gendiv_geo$Dist_Edge)
+linear_dist_hexp_lm_sum <- summary(linear_dist_hexp_lm)
+
+##now do with reduced 
+linear_dist_hexp_red_lm <- lm(butternut_26pop_gendiv_geo_red$HExp~butternut_26pop_gendiv_geo_red$Dist_Edge)
+linear_dist_hexp_red_lm_sum <- summary(linear_dist_hexp_red_lm)
+
+#####create data frame 
+butternut_26pop_hexp_rp_df <- matrix(nrow = 4, ncol = 4)
+
+##make a df
+butternut_26pop_hexp_rp_df[1,1:2] <- c(as.numeric(linear_lat_hexp_lm_sum[9]),
+                                       as.numeric(linear_lat_hexp_lm_sum$coefficients[2,4]))
+
+butternut_26pop_hexp_rp_df[2,1:2] <- c(as.numeric(linear_lat_hexp_red_lm_sum[9]),
+                                       as.numeric(linear_lat_hexp_red_lm_sum$coefficients[2,4]))
+
+butternut_26pop_hexp_rp_df[3,1:2] <- c(as.numeric(quad_lat_hexp_lm_sum[9]),
+                                       as.numeric(quad_lat_hexp_lm_sum$coefficients[2,4]))
+
+butternut_26pop_hexp_rp_df[4,1:2] <- c(as.numeric(quad_lat_hexp_model_red_lm_sum[9]),
+                                       as.numeric(quad_lat_hexp_model_red_lm_sum$coefficients[2,4]))
+
+butternut_26pop_hexp_rp_df[1,3:4] <- c(as.numeric(linear_dist_hexp_lm_sum[9]),
+                                       as.numeric(linear_dist_hexp_lm_sum$coefficients[2,4]))
+
+butternut_26pop_hexp_rp_df[2,3:4] <- c(as.numeric(linear_dist_hexp_red_lm_sum[9]),
+                                       as.numeric(linear_dist_hexp_red_lm_sum$coefficients[2,4]))
+
+write.csv(signif(butternut_26pop_hexp_rp_df, 3), "butternut_26pop_hexp_rp_df.csv")
 
 ##make plots 
 pdf("allrich_lat.pdf", width = 8, height = 6)
@@ -419,3 +444,92 @@ legend('bottom', legend = c("New Brunswick", "Ontario", "Quebec","United States"
        pch = 17, col = c("firebrick1", "firebrick4","lightsalmon","dodgerblue"))
 
 dev.off()
+
+########################################
+############## PCOA ####################
+########################################
+##first, code to reduce by individuals 
+butternut_26pop_relate_df <- Demerelate(butternut_26pop_lonlat[,-c(3:4)], object = T, value = "loiselle")
+
+##now identify how many individuals have greater than 25% relatedness = half siblings
+butternut_halfsib_names <- names(which(unlist(butternut_26pop_relate_df$Empirical_Relatedness) > 0.25))
+
+##then use this to create a document which has all the unique individual numbers for every highly related individuals
+butternut_halfsib_names_cleanfront <- gsub("^.*\\.","", butternut_halfsib_names)
+
+butternut_halfsib_names_cleanback <- gsub("^.*\\_","", butternut_halfsib_names_cleanfront)
+
+relate_ind_remove <- unique(butternut_halfsib_names_cleanback)
+
+##then subset genind file
+butternutgen_26pop_relatedness_reduced <- butternutgen_26pop[!rownames(butternutgen_26pop@tab) %in% relate_ind_remove,]
+
+##subset data frame 
+butternut_26pop_relatedness_reduced_df <- butternut_26pop_lonlat[!butternut_26pop_lonlat$Ind %in% relate_ind_remove,]
+
+##name pops in the genind file 
+levels(butternutgen_26pop_relatedness_reduced@pop) <- butternut_26pop_names
+
+##export genind 2 genalex 
+butternut_26pop_red_genalex <- genind2genalex(butternutgen_26pop_relatedness_reduced, 
+                                              filename = 'butternutgen_26pop_relatedness_reduced.csv')
+
+###########make PCoA 
+##run PCOA analysis 
+##now reorg into a genepop file 
+butternut_26pop_relate <- genind2genpop(butternutgen_26pop_relatedness_reduced)
+
+##run the PCA function
+butternut_reorg_pco <- dudi.pco(dist.genpop(butternut_26pop_relate, meth = 2), nf = 2, scannf = FALSE)
+
+##format df
+rownames(butternut_reorg_pco$li) <- butternut_26pop_names
+
+##re-org data for color coding
+butternut_pco_nb <- rbind(butternut_reorg_pco$li[c(1:6),1:2])
+butternut_pco_ot <- rbind(butternut_reorg_pco$li[c(7:11),1:2])
+butternut_pco_qu <- rbind(butternut_reorg_pco$li[c(12:13),1:2])
+butternut_pco_us <- rbind(butternut_reorg_pco$li[c(14:26),1:2])
+
+##Calculate percent variation explained 
+sum_eig <- sum(butternut_reorg_pco$eig)
+pc1 <- (butternut_reorg_pco$eig[[1]]/sum_eig)*100
+pc2 <- (butternut_reorg_pco$eig[[2]]/sum_eig)*100
+
+##PCOA of the reorg
+pdf("Genetic_Analyses\\genetic_analyses_results\\PCoA.pdf", width = 8, height = 6)
+##plot New Brunswick populations
+plot(butternut_pco_nb$A1, butternut_pco_nb$A2, pch = 17, 
+     xlab = paste0("PC1", sep = " ", "(",round(pc1, digits = 1), "%", ")"), 
+     ylab = paste0("PC2", sep = " ", "(",round(pc2, digits = 1), "%",")"), 
+     col = "firebrick1", xlim = c(-0.4, 0.4), 
+     ylim = c(-0.4, 0.4), cex = 1.2)
+##make axes
+abline(h = 0)
+abline(v = 0)
+##plot Ontario populations
+points(butternut_pco_ot$A1, butternut_pco_ot$A2, pch = 17, col = "firebrick4", cex = 1.2)
+
+##plot quebec populations 
+points(butternut_pco_qu$A1, butternut_pco_qu$A2, pch = 17, col = "lightsalmon", cex = 1.2)
+
+##plot US populations 
+points(butternut_pco_us$A1, butternut_pco_us$A2, col = "dodgerblue", pch = 17, cex = 1.2)
+
+##label Wisconsin populations 
+text(butternut_reorg_pco$li$A1[24], butternut_reorg_pco$li$A2[24], labels = "WI1", pos = 3, cex = 1.2)
+text(butternut_reorg_pco$li$A1[25], butternut_reorg_pco$li$A2[25], labels = "WI2", pos = 1, cex = 1.2)
+text(butternut_reorg_pco$li$A1[18], butternut_reorg_pco$li$A2[18], labels = "WI3", pos = 3, cex = 1.2)
+
+
+##create legend 
+legend('topleft', pch = 17, col = c("firebrick1","firebrick4", "lightsalmon", "dodgerblue"), 
+       legend = c("New Brunswick", "Ontario", "Quebec", "United States"), cex = 1.2)
+
+dev.off()
+
+##run session info 
+sessionInfo()
+
+
+
