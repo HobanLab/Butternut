ce81629be76b998d702f16e8d1b79a333ccf59bd
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2022-02-17 13:17:29 -0600

    Updating script with corrected naming scheme for butternut documents with missing data removed and reduced individuals for relatedness

--- genetic_analyses/RScripts/2_clean_genetic_data_remove_ind_relate.R
+++ genetic_analyses/RScripts/2_clean_genetic_data_remove_ind_relate.R
@@ -9,75 +9,68 @@
 #greater than 25% similar genotypes using the Loiselle statistic
 #before running PCoA and structure analysis.
 
-##########################
-######## Libraries #######
-##########################
+#####################
+#     Libraries     #
+#####################
 
 library(adegenet)
 library(Demerelate)
 library(poppr)
 
-#####################################
-############ Directories ############
-#####################################
-butternut_drive <- "C:\\Users\\eschumacher\\Documents\\GitHub\\Butternut"
+#######################
+#     Load Files      #
+#######################
+setwd("../data_files/after_reorg")
 
-#####################################
-############# Load Files ############
-#####################################
-setwd(butternut_drive)
-
-##load current working reorganized genepop file 
-butternutgen_reorg <- read.genepop("Genetic_Analyses\\data_files\\after_reorg\\postgeo_rebin_24pop.gen", ncode = 3)
+#load current working reorganized genepop file 
+butternutgen_reorg <- read.genepop("butternut_24pop.gen", ncode = 3)
 
 ##reduce genind file for individuals with greater than 25% missing data 
 butternutgen_nomd <- missingno(butternutgen_reorg, type = "geno", cutoff = 0.25, quiet = FALSE, freq = FALSE)
 
 ##load relatedness file 
-reorg_relatedness <- read.csv("Genetic_Analyses\\data_files\\after_reorg\\reorg_relatedness.csv")
+butternut_relate <- read.csv("butternut_24pop_relate.csv")
 
-###################################################################
-############# Reduced Individuals based on Relatedness ############
-###################################################################
-##rename individuals in genind file 
-rownames(butternutgen_nomd@tab) <- reorg_relatedness$Ind
+#####################################################
+#     Reduced Individuals based on Relatedness      #
+#####################################################
+#rename individuals in genind file 
+rownames(butternutgen_nomd@tab) <- butternut_relate$Ind
 
 ##create pop name code 
-butternut_24pop_names <- unique(reorg_relatedness$Pop)
+butternut_24pop_names <- unique(butternut_relate$Pop)
 
 ##rename populations in genind no missing data file 
 levels(butternutgen_nomd@pop) <- butternut_24pop_names
 
-####reduce relatedness
-##run relatedness code
-reorg_relate_df <- Demerelate(reorg_relatedness, object = T, value = "loiselle")
+##reduce individuals based on 25% relatedness
+#run relatedness code
+reorg_relate_df <- Demerelate(butternut_relate, object = T, value = "loiselle")
 
-##now identify how many individuals have greater than 25% relatedness = half siblings
+#now identify how many individuals have greater than 25% relatedness = half siblings
 butternut_halfsib_names <- names(which(unlist(reorg_relate_df$Empirical_Relatedness) > 0.25))
 
-##then use this to create a document which has all the unique individual numbers for every highly related individuals
+#then use this to create a document which has all the unique individual numbers for every highly related individuals
 butternut_halfsib_names_cleanfront <- gsub("^.*\\.","", butternut_halfsib_names)
 
 butternut_halfsib_names_cleanback <- gsub("^.*\\_","", butternut_halfsib_names_cleanfront)
 
 relate_ind_remove <- unique(butternut_halfsib_names_cleanback)
 
-##then subset genind file
-butternutgen_relatedness_reduced <- butternutgen_nomd[!rownames(butternutgen_nomd@tab) %in% relate_ind_remove,]
+#then subset genind object
+butternutgen_relate_red <- butternutgen_nomd[!rownames(butternutgen_nomd@tab) %in% relate_ind_remove,]
 
-##subset data frame 
-reorg_relatedness_reduced <- reorg_relatedness[!reorg_relatedness$Ind %in% relate_ind_remove,]
+#subset data frame 
+butternut_relate_red <- butternut_relate[!butternut_relate$Ind %in% relate_ind_remove,]
 
 ###name pops
 levels(butternutgen_relatedness_reduced@pop) <- butternut_24pop_names
  
-##write out genind file in genalex format
-setwd("Genetic_Analyses\\data_files\\after_reorg")
-butternutgen_relatedness_reduced <- genind2genalex(butternutgen_relatedness_reduced, 
-                                                   filename = "butternutgen_relatedness_reduced.csv")
-write.csv(reorg_relatedness_reduced, "reorg_relatedness_reduced.csv")
+#write out genind file in genalex format
+genind2genalex(butternutgen_relate_red, filename = "butternut_relate_red_genalex.csv")
+write.csv(butternut_relate_red, "butternut_relate_red.csv")
 
 ##write out no missing data genind file and then convert to genind in genalex
-butternutgen_nomd <- genind2genalex(butternutgen_nomd, filename = "butternutgen_nomd.csv")
+genind2genalex(butternutgen_nomd, filename = "butternut_24pop_nomd_genalex.csv")
 
 sessionInfo()
